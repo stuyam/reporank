9fcd96b9a093950fe9f2046c91837e3b1c3e529d
Stuart Yamartino <stuart@yamartino.com>
2016-02-26 23:28:50 -0500

    Deleted old default tables and added repo table

--- database/migrations/2016_02_27_012941_create_repos_table.php
+++ database/migrations/2016_02_27_012941_create_repos_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateReposTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('repos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('username');
+            $table->integer('rank');
+            $table->integer('stars');
+            $table->integer('forks');
+            $table->string('language');
             $table->timestamps();
         });
     }
@@ -29,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('repos');
     }
 }
