43946abd46df3793c04d0e78200695522cf1b4e1
Stuart Yamartino <stuart@yamartino.com>
2016-03-02 12:19:42 -0500

    Not include selecting the current repo for caching purposes

--- app/Http/Controllers/RepoController.php
+++ app/Http/Controllers/RepoController.php
@@ -11,13 +11,9 @@ use App\RepoRank\Badge;
 
 class RepoController extends Controller
 {
-  public function rank(Request $request, GitHub $github, Repo $repo, Badge $badge){
+  public function rank(Request $request, GitHub $github, Repo $repoDB, Badge $badge){
     $username = $request->username;
     $name     = $request->name;
-    return $this->handle($username, $name, $github, $repo, $badge);
-  }
-
-  private function handle($username, $name, $github, $repoDB, $badge){
     $repo = $repoDB->where('username', $username)->where('name', $name)->first();
     // if the repo exists in the database
     if($repo){
@@ -26,10 +22,10 @@ class RepoController extends Controller
         return $this->returnSVG($repo);
       }
       // if there are other repos with the same number of stars updated in the last 24 hours, use their rank
-      // else if($cachedRepo = $repoDB->where('stars', $repo->stars)->where('updated_at', '>=', (time() - 60*60*24))->orderBy('updated_at', 'DESC')->first()) {
-      //   $this->updateRepo($repo, $cachedRepo->rank, $cachedRepo->stars, $cachedRepo->badge);
-      //   return $this->returnSVG($repo);
-      // }
+      else if($cachedRepo = $this->hasSimilarRank($repoDB, $repo->stars, $username, $name)) {
+        $this->updateRepo($repo, $cachedRepo->rank, $cachedRepo->stars, $cachedRepo->badge);
+        return $this->returnSVG($repo);
+      }
       // get the updated repo info from github and update the database
       else {
         $gitRepo = $github->repo($username, $name);
@@ -62,6 +58,10 @@ class RepoController extends Controller
 
       return $this->returnSVG($repo);
     }
+  }
+
+  private function hasSimilarRank($repoDB, $stars, $username, $name){
+    return $repoDB->where('stars', $stars)->where('updated_at', '>=', (time() - 60*60*24))->where('username', '!=', $username)->where('name', '!=', $name)->orderBy('updated_at', 'DESC')->first();
   }
 
   private function updateRepo($repo, $rank, $stars, $badge){
